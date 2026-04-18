709ba94f81a71f42fb9dc8904c2f0b83bcb9ca19
Yeasin-Rifat <yeasinrifat1998@gmail.com>
2026-04-19 01:53:46 +0600

    First commit- project setup

---
+++ src/main/java/com/your_playground/YourPlaygroundApplication.java
@@ -0,0 +1,14 @@
+package com.your_playground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YourPlaygroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YourPlaygroundApplication.class, args);
+		System.out.println("PROJECT STARTS........................................");
+	}
+
+}
---
+++ src/test/java/com/your_playground/YourPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.your_playground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YourPlaygroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
